4d744f9856381785115ed85cce628337f4cb493b
nahian461-sudo <nahian461@gmail.com>
2026-06-24 12:28:29 +0600

    Delete symptoms.js

--- symptoms.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { submitSymptoms, getSubmission } = require('../controllers/symptomController');
-const { protect, roleGuard } = require('../middleware/auth');
-
-router.post('/', protect, roleGuard('patient'), submitSymptoms);
-router.get('/:id', protect, roleGuard('patient'), getSubmission);
-
-module.exports = router;
